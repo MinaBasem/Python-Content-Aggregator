0ab565d0f105e1335b729e67e0b9bc84bb8dbedc
Mina Basem <42482261+MinaBasem@users.noreply.github.com>
2024-05-26 22:11:46 +0300

    Final update

--- api_server.py
+++ api_server.py
@@ -11,12 +11,18 @@ import threading
 
 timezone = ''
 
+# -----------
+
 current_temp = 0
 current_feels_like_temp = 0
 current_rain_global = 0
 
 daily_temperature_max = 0
 daily_temperature_min = 0
+
+# -----------
+
+trending_topics = []
 
 # -----------
 
@@ -27,13 +33,15 @@ headline_url = []
 
 # -----------
 
-currencies = ['EUR', 'JPY', 'EGP', 'KWD', 'SAR']
+currencies = ['EUR', 'JPY', 'EGP']
 currency_pairs = []
 currency_pairs_values = []
+currency_dict = {}
 
 # -----------
 
-image_data = 0
+image_title = ''
+image_description = ''
 
 # -----------
 
@@ -41,14 +49,21 @@ number_fact = ''
 
 # -----------
 
-business_names = []
-business_type = []
-business_hours = []
+dog_fact = ''
+cat_fact = ''
 
 # -----------
 
-dog_fact = ''
-cat_fact = ''
+match_teams = []
+match_date = []
+match_time = []
+match_stadium = []
+
+# -----------
+
+job_titles = []
+job_company = []
+job_location = []
 
 
 def fetch_weather_api():
@@ -124,6 +139,31 @@ def fetch_weather_api():
 
     print(daily_dataframe)
     
+def fetch_twitter_api():
+    global trending_topics
+    url = "https://twitter154.p.rapidapi.com/trends/"
+
+    querystring = {"woeid":"1"}
+
+    headers = {
+        "X-RapidAPI-Key": "f35d6cc549msh5379362dc0450acp14361bjsn793713a82508",
+        "X-RapidAPI-Host": "twitter154.p.rapidapi.com"
+    }
+
+    response = requests.get(url, headers=headers, params=querystring)
+    response_text = response.content.decode("utf-8")
+    data_string = response_text[1:-1]   # remove [] from json
+    data = json.loads(data_string)
+
+    with open('response.json', 'w') as outfile:
+        json.dump(data, outfile)
+
+    with open('response.json') as infile:
+        data = json.load(infile)
+
+    trending_topics = [trend["name"] for trend in data['trends']]
+    return trending_topics
+
 def fetch_news_api():
 
     # The lists will store all obtained data. However, in the home page we will just add 5 headlines
@@ -149,7 +189,6 @@ def fetch_news_api():
 def fetch_stocks_api():
     import requests
     stocks = ['APPL', 'TSLA', 'AMZN']
-    currencies = ['EUR', 'JPY', 'EGP', 'KWD', 'SAR']
     primary_currency = 'USD'
     secondary_currency = random.choice(currencies)
 
@@ -168,85 +207,57 @@ def fetch_stocks_api():
     print(data)
 
 def fetch_forex_api():
-    primary_currency = 'USD'
-    api_key = 'VRY3M2RWH7KZW48N'
+    global currency_dict
 
-    for currency in currencies:
+    url = "https://currency-conversion-and-exchange-rates.p.rapidapi.com/latest"
+    #querystring = {"from":"EUR", "to":"EUR,JPY,EGP"}
+    headers = {
+        "X-RapidAPI-Key": "f35d6cc549msh5379362dc0450acp14361bjsn793713a82508",
+        "X-RapidAPI-Host": "currency-conversion-and-exchange-rates.p.rapidapi.com"
+    }
 
-        url = f'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={primary_currency}&to_currency={currency}&apikey={api_key}'
-        r = requests.get(url)
-        data = r.json()
+    response = requests.get(url, headers=headers)
+    data = response.json()
 
-        with open("finance.txt", "w") as file:
-            json.dump(data, file, indent=4)
+    usd_value = data["rates"].get("USD")
+    jpy_value = data["rates"].get("JPY")
+    eur_value = data["rates"].get("EGP")
 
-        #exchange_rate_symbol = data['Realtime Currency Exchange Rate']['1. From_Currency Code'] + '/' + data['Realtime Currency Exchange Rate']['3. To_Currency Code']
-        #exchange_rate_value = float(data['Realtime Currency Exchange Rate']['5. Exchange Rate'])
+    currency_dict = {'EUR/USD': usd_value, 'EUR/JPY': jpy_value, 'EUR/EGP': eur_value}
 
-        #currency_pairs.append(exchange_rate_symbol)
-        #currency_pairs_values.append(exchange_rate_value)
-
-
-    #print(currency_pairs)
-    #print(currency_pairs_values)
+    return currency_dict
 
 def fetch_image_of_the_day():
     api_key = 'olPDz85SxHhuZfK7vSAY7vocjSQhnrIlglY64O7N'
-
     url = f"https://api.nasa.gov/planetary/apod?api_key={api_key}"
 
-    # Send the GET request and store the response
     response = requests.get(url)
+    data = response.json()
+    
+    image_url = data["url"]
+    image_title = data["title"]
+    image_description = data["explanation"]
+    
+    print(f"Image URL: {image_url}")
+    print(f"Title: {image_title}")
+    print(f"Explanation: {image_description}")
 
-    # Check for successful response (status code 200)
-    if response.status_code == 200:
-        # Convert the response to JSON format
-        data = response.json()
-        
-        image_url = data["url"]
-        title = data["title"]
-        explanation = data["explanation"]
-        
-        print(f"Image URL: {image_url}")
-        print(f"Title: {title}")
-        print(f"Explanation: {explanation}")
+    response = requests.get(image_url)
+    image_data = response.content
 
-        response = requests.get(image_url)
-        image_data = response.content
-        filename = "image_of_the_day.jpg"
+    with open("image_of_the_day.jpg", "wb") as file:
+        file.write(image_data)
 
-        with open(filename, "wb") as file:
-            file.write(image_data)
+    print(f"Image downloaded successfully as 'image_of_the_day.jpg'")
 
-        print(f"Image downloaded successfully as {filename}")
-    else:
-        print(f"Error: {response.status_code}")
+    return image_title, image_description
 
 def fetch_number_fact_of_the_day():
     api_url = "http://numbersapi.com/random/trivia?json"
     response = requests.get(api_url)
     data = response.json()
-    fact = data.get("text")
-    print("FACT: -------> " + str(fact))
-    return fact
-
-def fetch_local_businesses_data():
-    url = "https://local-business-data.p.rapidapi.com/search"
-
-    querystring = {"query":"supermarkets","lat":"30.0626","lng":"31.2497","limit":"3","language":"en","region":"us"}
-    
-    headers = {
-        "X-RapidAPI-Key": "f35d6cc549msh5379362dc0450acp14361bjsn793713a82508",
-        "X-RapidAPI-Host": "local-business-data.p.rapidapi.com"
-    }
-
-    response = requests.get(url, headers=headers, params=querystring)
-    data = response.json()
-
-    with open("nearby_places.json", "w") as file:
-        json.dump(data, file, indent=4)
-
-    print(response.json())
+    number_fact = data.get("text")
+    return number_fact
 
 def fetch_random_famous_quote():
     url = "https://twitter154.p.rapidapi.com/user/details"
@@ -260,6 +271,28 @@ def fetch_random_famous_quote():
 
     print(response.json())
 
+def fetch_football_api():
+
+    from datetime import date, timedelta
+
+    today = date.today()
+    one_week_from_today = today + timedelta(days=3)
+
+    api_key = "2b69fb2e142a35a9304bd95df97f1958f2aa8184e2ff4f8121219ed0f91a18c5"
+    url = f"https://apiv2.allsportsapi.com/football/?met=Fixtures&APIkey={api_key}&from={today}&to={one_week_from_today}"
+
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        for item in data["result"]:
+            match_date.append(item["event_date"])
+            match_time.append(item["event_time"])
+            match_stadium.append(item["event_stadium"])
+            match_teams.append(f"{item['event_home_team']} VS {item['event_away_team']}")
+        
+        return match_teams, match_date, match_time, match_stadium
+    else:
+        print("Error:", response.status_code)
 
 def fetch_random_dog_cat_fact():
     url = "https://random-dog-facts.p.rapidapi.com/"
@@ -271,7 +304,10 @@ def fetch_random_dog_cat_fact():
 
     response = requests.get(url, headers=headers)
     fact = response.json()
-    dog_fact = fact.get("message")
+    if "fact" in fact:
+        dog_fact = fact["fact"]
+    else:
+        dog_fact = fact.get("message", "Error retrieving dog fact.")
 
     url = "https://random-cat-fact.p.rapidapi.com/"
 
@@ -282,21 +318,90 @@ def fetch_random_dog_cat_fact():
 
     response = requests.get(url, headers=headers)
     fact = response.json()
-    cat_fact = fact.get("message")
+    if "fact" in fact:
+        cat_fact = fact["fact"]
+    else:
+        cat_fact = fact.get("message", "Error retrieving cat fact.")
 
     return dog_fact, cat_fact
 
-# Create and start threads in a separate function
-def start_data_threads():
+def fetch_linkedIn_jobs():
+    url = "https://linkedin-bulk-data-scraper.p.rapidapi.com/search_jobs"
+
+    querystring = {"query":"internship","location":"Egypt","page":"1"}
+
+    headers = {
+        "X-RapidAPI-Key": "f35d6cc549msh5379362dc0450acp14361bjsn793713a82508",
+        "X-RapidAPI-Host": "linkedin-bulk-data-scraper.p.rapidapi.com"
+    }
+
+    response = requests.get(url, headers=headers, params=querystring)
+
+    data = response.json()
+
+    for job in data["response"]["jobs"]:
+        job_titles.append(job["title"])
+        job_company.append(job["primaryDescription"])
+        job_location.append(job["secondaryDescription"])
+
+    with open('linkedin_response.json', 'w') as outfile:
+        json.dump(data, outfile)
+
+    return job_titles, job_company, job_location
+
+def save_variables_to_file(exclude_imports=True):
+
+    from types import ModuleType
+
+    with open("variables.py", "w", encoding="utf-8") as file:
+
+        for var_name, var_value in globals().items():
+            if exclude_imports and isinstance(var_value, (type, ModuleType)) or var_name in ("threads", "thread"):
+                continue  # Skip imported modules
+            if not var_name.startswith('__') and not callable(var_value):
+                file.write(f"{var_name} = {repr(var_value)}\n")
+
+        print("# Generated file with variables and their values\n")
+
+def send_variables_file():
+    HOST = '127.0.0.1'
+    PORT = 65432
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+        server_socket.bind((HOST, PORT))
+        server_socket.listen()
+
+        print("Server is listening for connections...")
+
+        while True:
+            client_socket, client_address = server_socket.accept()
+            print(f"Connection established with {client_address}")
+
+            try:
+                with open("variables.py", "rb") as file:
+                    file_data = file.read()
+
+                client_socket.sendall(file_data)
+                print("File sent successfully to the client.")
+
+                client_socket.close()
+
+            except Exception as e:
+                print(f"Error occurred while sending file: {e}")
+
+if __name__ == "__main__":
+
+    image_title, image_description = fetch_image_of_the_day()
+
     threads = [
         threading.Thread(target=fetch_weather_api),
+        threading.Thread(target=fetch_twitter_api),
         threading.Thread(target=fetch_news_api),
-        #threading.Thread(target=fetch_stocks_api),
         threading.Thread(target=fetch_forex_api),
         #threading.Thread(target=fetch_image_of_the_day),
-        #threading.Thread(target=fetch_number_fact_of_the_day),
-        #threading.Thread(target=fetch_local_businesses_data),
-        #threading.Thread(target=fetch_random_dog_cat_fact)
+        threading.Thread(target=fetch_random_dog_cat_fact),
+        threading.Thread(target=fetch_football_api),
+        threading.Thread(target=fetch_linkedIn_jobs),
     ]
 
     for thread in threads:
@@ -305,7 +410,7 @@ def start_data_threads():
     for thread in threads:
         thread.join()
 
-if __name__ == "__main__":
-    start_data_threads()
+    save_variables_to_file()
+    send_variables_file()
 
     
--- test.py
+++ test.py
@@ -1,90 +1,21 @@
-import pandas as pd
-import time
-from socket import *
-import os
-import sys
-from PyQt5.QtCore import (Qt, QSize, QUrl)
-from PyQt5.QtWidgets import (
-    QApplication,
-    QHBoxLayout,
-    QLabel,
-    QPushButton,
-    QVBoxLayout,
-    QWidget,
-    QListWidget,
-    QLineEdit,
-    QFrame,
-    QListWidgetItem,
-    QMessageBox,
-    QScrollArea
-)
+from PyQt5.QtCore import QUrl
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWebEngineWidgets import QWebEngineView
 
-## Create UI
 
-class MainWindow(QWidget):
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.init_ui()
+        self.web_view = QWebEngineView(self)
+        html_content = """
+        <html><body>
+            <h1>Hello, PyQt5!</h1>
+        </body></html>"""
+        self.web_view.setHtml(html_content)
+        self.setCentralWidget(self.web_view)
 
-    def init_ui(self):
-
-        self.setWindowTitle("File Transfer")
-        self.setFixedSize(700, 500)
-
-        self.search_box = QLineEdit()
-        self.search_box.setPlaceholderText("Search...")
-        self.search_button = QPushButton("Search")
-        #self.search_button.clicked.connect(self.search)
-
-        top_layout = QHBoxLayout()
-        top_layout.addWidget(self.search_box)
-        top_layout.addWidget(self.search_button)
-
-        self.content_widget = QWidget()
-        content_layout = QVBoxLayout(self.content_widget)
-        content_layout.setAlignment(Qt.AlignTop)
-
-        label_1 = QLabel("Label 1")
-        label_2 = QLabel("Label 2")
-        label_3 = QLabel("Label 3")
-        
-        button1 = QPushButton("Button 1")
-        button2 = QPushButton("Button 2")
-        button3 = QPushButton("Button 3")
-
-        self.setStyleSheet("QLabel, QPushButton, QFrame { color: White; }")
-        #self.setStyleSheet("QPushButton { color: White; }")
-
-        content_layout.addWidget(label_1)
-        content_layout.addWidget(button1)
-        content_layout.addWidget(QFrame(frameShape=QFrame.HLine))
-
-        content_layout.addWidget(label_2)
-        content_layout.addWidget(button2)
-        content_layout.addWidget(QFrame(frameShape=QFrame.HLine))
-
-        content_layout.addWidget(label_3)
-        content_layout.addWidget(button3)
-        content_layout.addWidget(QFrame(frameShape=QFrame.HLine))
-
-        #self.layout.addWidget(self.scroll_area)
-
-        self.scroll_area = QScrollArea()
-        self.scroll_area.setStyleSheet("background-color: black;")
-        self.scroll_area.setWidget(self.content_widget)
-        self.scroll_area.setWidgetResizable(True)
-
-        main_layout = QVBoxLayout()
-        main_layout.addLayout(top_layout)
-        main_layout.addWidget(self.scroll_area)
-
-        self.setLayout(main_layout)
-        self.show()
-
-
-## Create functions for displaying each kind of scraped content
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
+if __name__ == '__main__':
+    app = QApplication([])
     window = MainWindow()
-    sys.exit(app.exec_())
+    window.show()
+    app.exec_()
